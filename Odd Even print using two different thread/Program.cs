b62fa1d8844338dff02f918005b4f9cb976f6c6c
jd_786 <jaydeep_shil@certree.com>
2024-02-14 19:37:54 +0530

    Add project files.

---
+++ Odd Even print using two different thread/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace Odd_Even_print_using_two_different_thread
+{
+    internal class Program
+    {
+        static AutoResetEvent oddevent = new AutoResetEvent(false);
+        //static AutoResetEvent evenevent = new AutoResetEvent(false);
+        static int index = 1;
+        static void Main(string[] args)
+        {
+            Thread odd = new Thread(PrintOdd);
+            Thread even = new Thread(PrintEven);
+            even.Start();
+            odd.Start();
+            
+        }
+
+        static void PrintOdd()
+        {
+            while (index <= 100)
+            {
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} print {index++}");
+                //evenevent.Set();
+                oddevent.Set();
+                oddevent.WaitOne();
+            }
+        }
+
+        static void PrintEven()
+        {
+            while (index <= 100) 
+            {
+                //evenevent.WaitOne();
+                oddevent.WaitOne();
+                Console.WriteLine($"Thread {Thread.CurrentThread.ManagedThreadId} print {index++}");
+                oddevent.Set();
+            }
+        }
+    }
+}
